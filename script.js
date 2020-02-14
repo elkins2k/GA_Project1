9357a104a827df7184696c8d36a085bf9067545a
Jeff Elkins <elkins2k@mail.com>
2020-02-14 10:32:57 -0600

    commit6

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log(`script.js connected!`)
-
 /*
 technical enablement of game logic
 - each stack is an array
@@ -9,7 +7,10 @@ technical enablement of game logic
 - to start, array1 = [ 7, 6, 5, 4, 3, 2, 1]
 - to win, array3 = [ 7, 6, 5, 4, 3, 2, 1]
 */
-
+let stack1 = []
+let stack2 = []
+let stack3 = []
+const forTheWin = [ 7, 6, 5, 4, 3, 2, 1 ]
 /*
 create playing area with buttons
 */
@@ -20,7 +21,45 @@ allow user to enter number of disks for game (3-7)
 set each stack array and populate first array with number of disks
 create gameboard with number of disks chosen by user
 */
+function initializeGame () {
+    const headerElements = document.querySelectorAll (`header`)
+    //clear out the header 
+    while (headerElements[0].lastElementChild)
+        headerElements[0].removeChild ( headerElements[0].lastElementChild )
+    //create new header elements to get user input
+    //create form element
+    const headerForm = document.createElement ("form")
+    headerForm.className = "headerForm"
+    headerForm.innerHTML = (`Please select a game to play: `)
+    headerElements[0].appendChild(headerForm)
+    //ceate radio button inputs
+    for (i=3; i<=7; i++) {
+        let headerInput = document.createElement ("input")
+        headerInput.setAttribute(
+            {"type":"radio"})
+        headerInput.setAttribute("id" , `${i}`)
+        headerInput.setAttribute("name" , "diskCount")
+        headerInput.setAttribute("value" , `${i}`)
+        console.log(headerInput)
+        headerForm.appendChild (headerInput)
+        let headerLabel = document.createElement (   
+            "label", {"for" : `${i}`}
+        )
+        console.log(headerLabel)
+        headerForm.appendChild (headerLabel)
+        headerLabel.innerHTML = (`${i} disks`)
+    }
+    
 
+    
+    
+
+    const message = `<p> How many disks would you like to try? (enter a # between 3 and 7) </p>`
+    headerDiv.outerText (message)
+    stack1 = [ 7, 6, 5, 4, 3, 2, 1 ]
+    stack2 = [ ]
+    stack3 = [ ]
+}
 /*
 f = on click, set sourceArray
 on next click, set targetArray
@@ -40,3 +79,4 @@ if array3 != [ 7, 6, 5, 4, 3, 2, 1]
     else WIN!!,
     initialize game
 */
+initializeGame();
